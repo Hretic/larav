a521db80297be4dea0ef28be3b108d206f672f54
Hretic <maxxxir@gmail.com>
2022-06-18 20:31:35 +0430

    api/home added

---
+++ app/Http/Controllers/Api/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    function index(){
+        return response()->json(['stat'=>'ok' , 'data'=>'home']);
+    }
+}
